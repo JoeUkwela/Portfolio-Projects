bb1b1010c5a0cba93cb77ef60e93b7b8b3ab3abe
Ukwela Joshua <ukwelajoshua@outlook.com>
2024-04-02 07:30:07 +0100

    Create script.js

---
+++ Drum Kit/js/script.js
@@ -0,0 +1,57 @@
+for(var i = 0; i < document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",
+    function(){
+        var expression = this.innerHTML;
+        makeSound(expression);
+        buttonAnimation(expression);
+        
+    });
+}
+
+document.addEventListener("keydown",function(event){
+   makeSound(event.key);
+   buttonAnimation(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        
+        default:
+            console.log(this.innerHTML);
+    }
+}
+
+function buttonAnimation(currentKey){
+   var active_btn =  document.querySelector("."+currentKey);
+   active_btn.classList.add("pressed");
+   setTimeout(function(){
+    active_btn.classList.remove("pressed");},100)
+}
